9feb54de331d54c1987cf576314966e53fdfcc64
StiviiK <stefan.kuerzeder@gmail.com>
2020-08-14 14:42:04 +0200

    Addendum to last commit

--- ui/src/hud/vehicle-ui.jsx
+++ ui/src/hud/vehicle-ui.jsx
@@ -56,23 +56,23 @@ class VehicleUI extends Component {
                 <div className="select-none">
                     <div className="flex mt-48">
                         <div type="vehInfo" className="group mt-32 ml-auto mr-32 w-32 h-32 bg-blue-500 rounded-full border-4 border-blue-500 opacity-75 shadow-xl hover:border-4 hover:border-gray-200" onMouseOver={this.hover}>
-                            <img className="w-24 h-24 mx-auto mt-3" src="https://static.exo.cool/exov-static/images/vehicles/interaction/info.png"></img>
+                            <img className="w-24 h-24 mx-auto mt-3 shadow-img" src="https://static.exo.cool/exov-static/images/vehicles/interaction/info.png"></img>
                         </div>
                         <div type="light" className="mt-32 mr-auto w-32 h-32 bg-blue-500 rounded-full border-4 border-blue-500 opacity-75 shadow-xl hover:border-4 hover:border-gray-200" onMouseOver={this.hover}>
-                            <img className="w-24 h-24 ml-3 mt-2" src="https://static.exo.cool/exov-static/images/vehicles/interaction/light.png"></img>
+                            <img className="w-24 h-24 ml-3 mt-2 shadow-img" src="https://static.exo.cool/exov-static/images/vehicles/interaction/light.png"></img>
                         </div>
                     </div>
                     <p className="relative mt-6 text-center text-bold text-gray-200 text-3xl mx-auto">{this.state.currentSelection}</p>
                     <div className="flex">
                         <div type="lock" className="ml-auto mr-64 w-32 h-32 bg-blue-500 rounded-full border-4 border-blue-500 opacity-75 shadow-xl hover:border-4 hover:border-gray-200" onMouseOver={this.hover}>
-                            <img className="w-20 h-20 mx-auto mt-5" src="https://static.exo.cool/exov-static/images/vehicles/interaction/key.png"></img>
+                            <img className="w-20 h-20 mx-auto mt-5 shadow-img" src="https://static.exo.cool/exov-static/images/vehicles/interaction/key.png"></img>
                         </div>
                         <div type="engine" className="mr-auto w-32 h-32 bg-blue-500 rounded-full border-4 border-blue-500 opacity-75 shadow-xl hover:border-4 hover:border-gray-200" onMouseOver={this.hover}>
-                            <img className="w-24 h-24 mx-auto mt-2" src="https://static.exo.cool/exov-static/images/vehicles/interaction/engine.png"></img>
+                            <img className="w-24 h-24 mx-auto mt-2 shadow-img" src="https://static.exo.cool/exov-static/images/vehicles/interaction/engine.png"></img>
                         </div>
                     </div>
                     <div type="close" className="mx-auto w-32 h-32 bg-blue-500 rounded-full border-4 border-blue-500 opacity-75 shadow-xl hover:border-4 hover:border-gray-200" onMouseOver={this.hover}>
-                        <img className="mx-auto w-24 h-24 mt-3" src="https://static.exo.cool/exov-static/images/vehicles/interaction/close.png"></img>
+                        <img className="mx-auto w-24 h-24 mt-3 shadow-img" src="https://static.exo.cool/exov-static/images/vehicles/interaction/close.png"></img>
                     </div>
                 </div>
             )
@@ -81,23 +81,23 @@ class VehicleUI extends Component {
                 <div className="select-none">
                     <div className="flex mt-48">
                         <div type="vehInfo" className="group mt-32 ml-auto mr-32 w-32 h-32 bg-blue-500 rounded-full border-4 border-blue-500 opacity-75 shadow-xl hover:border-4 hover:border-gray-200" onMouseOver={this.hover}>
-                            <img className="w-24 h-24 mx-auto mt-3" src="https://static.exo.cool/exov-static/images/vehicles/interaction/info.png"></img>
+                            <img className="w-24 h-24 mx-auto mt-3 shadow-img" src="https://static.exo.cool/exov-static/images/vehicles/interaction/info.png"></img>
                         </div>
                         <div type="trunk" className="mt-32 mr-auto w-32 h-32 bg-blue-500 rounded-full border-4 border-blue-500 opacity-75 shadow-xl hover:border-4 hover:border-gray-200" onMouseOver={this.hover}>
-                            <img className="w-24 h-24 ml-3 mt-3" src="https://static.exo.cool/exov-static/images/vehicles/interaction/trunk.png"></img>
+                            <img className="w-24 h-24 ml-3 mt-3 shadow-img" src="https://static.exo.cool/exov-static/images/vehicles/interaction/trunk.png"></img>
                         </div>
                     </div>
                     <p className="relative mt-6 text-center text-bold text-gray-200 text-3xl mx-auto">{this.state.currentSelection}</p>
                     <div className="flex">
                         <div type="lock" className="ml-auto mr-64 w-32 h-32 bg-blue-500 rounded-full border-4 border-blue-500 opacity-75 shadow-xl hover:border-4 hover:border-gray-200" onMouseOver={this.hover}>
-                            <img className="w-20 h-20 mx-auto mt-5" src="https://static.exo.cool/exov-static/images/vehicles/interaction/key.png"></img>
+                            <img className="w-20 h-20 mx-auto mt-5 shadow-img" src="https://static.exo.cool/exov-static/images/vehicles/interaction/key.png"></img>
                         </div>
                         <div type="engineHood" className="mr-auto w-32 h-32 bg-blue-500 rounded-full border-4 border-blue-500 opacity-75 shadow-xl hover:border-4 hover:border-gray-200" onMouseOver={this.hover}>
-                            <img className="w-24 h-24 mx-auto mt-2" src="https://static.exo.cool/exov-static/images/vehicles/interaction/engineHood.png"></img>
+                            <img className="w-24 h-24 mx-auto mt-2 shadow-img" src="https://static.exo.cool/exov-static/images/vehicles/interaction/engineHood.png"></img>
                         </div>
                     </div>
                     <div type="close" className="mx-auto w-32 h-32 bg-blue-500 rounded-full border-4 border-blue-500 opacity-75 shadow-xl hover:border-4 hover:border-gray-200" onMouseOver={this.hover}>
-                        <img className="mx-auto w-24 h-24 mt-3" src="https://static.exo.cool/exov-static/images/vehicles/interaction/close.png"></img>
+                        <img className="mx-auto w-24 h-24 mt-3 shadow-img" src="https://static.exo.cool/exov-static/images/vehicles/interaction/close.png"></img>
                     </div>
                 </div>
             )
