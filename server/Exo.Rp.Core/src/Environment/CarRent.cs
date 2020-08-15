a64b9aa22a577a1e716b685f4fa4251d55c8910b
FiNAL <fnlx089@gmail.com>
2020-08-15 17:28:09 +0200

    Insert ~72 pizza delivery spots - Thanks to Refrigerator

---
+++ server/Exo.Rp.Core/src/Environment/CarRent.cs
@@ -0,0 +1,24 @@
+using AltV.Net.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace server.Environment
+{
+	internal class CarRent
+	{
+		private Position[] rentSpots =
+		{
+			new Position(),
+		};
+		public CarRent()
+		{
+			LoadCarRents();
+		}
+
+		public void LoadCarRents()
+		{
+
+		}
+	}
+}
