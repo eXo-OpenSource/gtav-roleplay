d2298ac75efbac561b1958896452b59fcd21b727
DeeJayBro <info@deejaybro.de>
2020-08-16 07:59:13 +0200

    import non class files

--- client/src/Core.ts
+++ client/src/Core.ts
@@ -2,10 +2,10 @@ import * as alt from "alt-client";
 import { Singleton } from "./utils/Singleton";
 import { UiManager } from './ui/UiManager';
 // import { log } from "util";
-import {Vehicle} from "./systems/Vehicle";
-import {Notification} from "./systems/Notification";
+import "./systems/Vehicle";
+import "./systems/Notification";
 import Interaction from "./systems/Interaction";
-import Streamer from "./systems/Streamer";
+import "./systems/Streamer";
 import WasteCollector from "./jobs/WasteCollector";
 import LawnMower from "./jobs/LawnMower";
 import PizzaDelivery from "./jobs/PizzaDelivery";
@@ -14,11 +14,8 @@ import DoorManager from "./world/Doormanager";
 
 @Singleton
 export class Core {
-    private vehicle = new Vehicle();
-    private notification = new Notification();
     private uiManager = new UiManager();
     private interaction = new Interaction(this.uiManager);
-    private streamer = new Streamer();
     private waste = new WasteCollector();
     private lawn = new LawnMower();
     private pizza = new PizzaDelivery();
