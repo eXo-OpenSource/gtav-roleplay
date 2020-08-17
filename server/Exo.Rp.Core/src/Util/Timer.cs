448b5f237f93e8650e826fe70a7a3b6aa1373bd5
FiNAL <fnlx089@gmail.com>
2020-08-17 23:15:46 +0200

    Add timer class

--- server/Exo.Rp.Core/src/Util/Timer.cs
+++ server/Exo.Rp.Core/src/Util/Timer.cs
@@ -1,10 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
-namespace server.src.Util
+namespace server.Util
 {
-    class Timer
+    public class TimerHandler
     {
+
+        // Use for example like: this.timer = new Util.TimerHandler(() =>{this.giveGo();}, 222);
+
+        private Timer timer { get; set; }
+        private Action callback { get; set; }
+
+        public TimerHandler(Action callback, int milliseconds, int repeat = Timeout.Infinite)
+        {
+            this.callback = callback;
+
+            this.timer = new Timer(
+                new TimerCallback(TimerCallback),
+                null,
+                milliseconds,
+                repeat);
+        }
+
+        public void StopTimeout()
+        {
+            timer.Change(Timeout.Infinite, Timeout.Infinite);
+
+        }
+
+        private void TimerCallback(object state)
+        {
+            this.callback();
+            timer.Change(
+                Timeout.Infinite,
+                Timeout.Infinite);
+        }
+
     }
 }
