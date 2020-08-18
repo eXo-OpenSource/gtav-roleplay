0635537cd75eb3733cea49b6d9f72119f3a79b1f
StiviiK <stefan.kuerzeder@gmail.com>
2020-08-18 09:33:21 +0000

    Enable core dump collection in container

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+# Enable core dump collection (https://dev.to/mizutani/how-to-get-core-file-of-segmentation-fault-process-in-docker-22ii)
+echo '/tmp/core.%h.%e.%t' > /proc/sys/kernel/core_pattern
+ulimit -c unlimited
+
+# Start server
 ./altv-server --config=/opt/altv/server.cfg
