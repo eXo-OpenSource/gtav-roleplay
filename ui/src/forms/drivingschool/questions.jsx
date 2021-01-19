4dd24345ada80ee66abcf80130a862b3b133265b
FiNAL <fnlx089@gmail.com>
2021-01-19 23:37:29 +0100

    Add quiz component

---
+++ ui/src/forms/drivingschool/questions.jsx
@@ -0,0 +1,48 @@
+export var drivingSchoolQuestions = [
+    {
+      id: 1,
+      text: 'Frage sasdasds1',
+      choices: [
+        {
+          id: 'A',
+          text: 'Antwort 1'
+        },
+        {
+          id: 'B',
+          text: 'Antwort 2'
+        },
+        {
+          id: 'C',
+          text: 'Antwort 3'
+        },
+        {
+          id: 'D',
+          text: 'Antwort 4'
+        }
+      ],
+      correct: 'C'
+    },
+    {
+      id: 2,
+      text: 'Frage dasdas2',
+      choices: [
+        {
+          id: 'A',
+          text: 'Antwort 1'
+        },
+        {
+          id: 'B',
+          text: 'Antwort 2'
+        },
+        {
+          id: 'C',
+          text: 'Antwort 3'
+        },
+        {
+          id: 'D',
+          text: 'Antwort 4'
+        }
+      ],
+      correct: 'C'
+    },
+  ]
