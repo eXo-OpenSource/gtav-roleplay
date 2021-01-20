364f63b6159f1ca2fb3c1f240e73b2f8d3125563
Remember <remembot21@gmail.com>
2021-01-20 15:32:44 +0100

    add dev command

--- client/src/utils/DevCommands.ts
+++ client/src/utils/DevCommands.ts
@@ -2,19 +2,22 @@ import * as alt from "alt-client";
 import * as native from "natives";
 
 var positions = [];
-var collected = 0
-var player = alt.Player.local
+var rotations = [];
+var collected = 0;
+var player = alt.Player.local;
 
 alt.on("keyup", (key) => {
   switch (key) {
     case 0x73:
-      positions.push("new Position(" + player.pos.x + "f, " + player.pos.y + "f, " + player.pos.z + "f)\n")
-      alt.emitServer("Chat:Message", "[ID: " + (positions.length - 1) + "] Position hinzugefügt")
-      break
+      positions.push("new Position(" + player.pos.x + "f, " + player.pos.y + "f, " + player.pos.z + "f)\n");
+      rotations.push("new Rotation(" + player.rot.x + "f, " + player.rot.y + "f, " + player.rot.z + "f)\n");
+      alt.emitServer("Chat:Message", "[ID: " + (positions.length - 1) + "] Position hinzugefügt");
+      break;
     case 0x72:
-      if (positions.length < 1) break
-      alt.emitServer("Chat:Message", "[ID: " + (positions.length - 1) + "] Position gelöscht")
-      positions.pop()
+      if (positions.length < 1) break;
+      alt.emitServer("Chat:Message", "[ID: " + (positions.length - 1) + "] Position gelöscht");
+      positions.pop();
+      rotations.pop();
       break
   }
 })
@@ -22,15 +25,24 @@ alt.on("keyup", (key) => {
 alt.on("consoleCommand", (cmd, ...args) => {
   switch (cmd) {
     case "positions":
-      var resString = positions.join("")
-      alt.log(resString)
-      break
+      var resString = "Keine Positionen gespeichert";
+      if (args.length > 0 && args[0] == "true") {
+        resString = "";
+        for (var i = 0; i < positions.length; i++) {
+          resString += "{" + positions[i].replace("\n", ",") + " " + rotations[i].replace("\n", "") + "},\n";
+        }
+      } else {
+        resString = positions.join("");
+      }
+      alt.log(resString);
+      break;
     case "resetpos":
-      positions = []
-      alt.emitServer("Chat:Message", "Alle Positionen gelöscht!")
-      break
+      positions = [];
+      rotations = [];
+      alt.emitServer("Chat:Message", "Alle Positionen gelöscht!");
+      break;
     default:
-      alt.emitServer("Chat:Message", "Command " + cmd + " not found!")
-      break
+      alt.emitServer("Chat:Message", "Command " + cmd + " not found!");
+      break;
   }
 })
