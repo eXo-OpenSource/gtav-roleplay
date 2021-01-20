a5ceeb930fb9e829f2e4f0bc5be6d87f11917e88
StiviiK <stefan.kuerzeder@gmail.com>
2021-01-20 15:19:57 +0100

    fixed hex string parsing

--- server/Exo.Rp.Core/src/Util/General.cs
+++ server/Exo.Rp.Core/src/Util/General.cs
@@ -36,8 +36,7 @@ namespace Exo.Rp.Core.Util
         
         public static bool IsHexString(string test)
         {
-            // For C-style hex notation (0xFF) you can use @"\A\b(0[xX])?[0-9a-fA-F]+\b\Z"
-            return System.Text.RegularExpressions.Regex.IsMatch(test, @"\A\b[0-9a-fA-F]+\b\Z");
+            return System.Text.RegularExpressions.Regex.IsMatch(test, @"\A\b(0[xX])?[0-9a-fA-F]+\b\Z");
         }
     }
 }
